9f8d752f4844b8047de75c704a0050c935243955
Sly <85970858+SlyNinjutsu07@users.noreply.github.com>
2026-07-11 16:38:18 -0400

    refactor: basic clean up from previous UI attempt; sticking to JTree

--- src/main/java/com/desktopbuddy/ui/NoteManager.java
+++ src/main/java/com/desktopbuddy/ui/NoteManager.java
@@ -1,17 +1,13 @@
 package com.desktopbuddy.ui;
 
 import com.desktopbuddy.data.*;
-import com.sun.source.tree.Tree;
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.nio.file.*;
 
 public class NoteManager {
@@ -25,12 +21,6 @@ public class NoteManager {
     //SETTINGS BUTTON
     private JButton settingsButton;
     private SettingsData settingsData;
-
-    //CONTENT
-    private JPanel contentPanel;
-    //TODO: add field for the current path that is being viewed
-    private ArrayList<Folder> folders = new ArrayList<>();
-    private ArrayList<Note> notes = new ArrayList<>();
 
     //JTREE
     private JTree tree;
@@ -64,17 +54,11 @@ public class NoteManager {
         //tree renderer
         initializeTreeRenderer();
 
-        contentPanel = new JPanel();
-        contentPanel.setLayout(new BorderLayout());
-        contentPanel.add(initFolders());
-        contentPanel.add(initNotes());
-
         //add content to manager window
         window.add(toolbar, BorderLayout.NORTH);
 
         Folder dir = new Folder(rootDir.getFolderName(), rootDir.getFolderPath());
         window.add(new JScrollPane(tree), BorderLayout.CENTER);
-        //window.add(contentPanel, BorderLayout.CENTER);
     }
 
     private void initializeTreeRenderer(){
@@ -93,107 +77,6 @@ public class NoteManager {
         renderer.setClosedIcon(new ImageIcon(scaledFolderIcon));
         tree.setCellRenderer(renderer);
     }
-
-
-    /*
-
-        EVERYTHING BELOW IS DEAD CODE BECAUSE I'M USING A JTREE INSTEAD
-
-     */
-
-
-
-    //refresh the contentPanel with new content after accessing root
-    private void refreshWindow(Folder newRoot){
-        fillItems(newRoot);
-        contentPanel.removeAll();
-        contentPanel.add(initFolders(), BorderLayout.NORTH);
-        contentPanel.add(initNotes(), BorderLayout.CENTER);
-
-        //For re-orienting and re-drawing everything live
-        contentPanel.revalidate();
-        contentPanel.repaint();
-    }
-
-    //Fills the array lists with the current view (root) of contents
-    private void fillItems(Folder root){
-
-        //clear current view
-        folders.clear();
-        notes.clear();
-
-        //read from the path and figure out the folders and notes and add them
-        try (var stream = Files.list(root.getFolderPath())){
-            stream.forEach(itemPath -> {
-                String itemName = itemPath.getFileName().toString();
-                if(Files.isDirectory(itemPath)){
-                    if(itemName.equals(".obsidian")) return;
-                    folders.add(new Folder(itemName, itemPath));
-                } else if (itemName.endsWith(".md")) {
-                    notes.add(new Note(itemName, itemPath));
-                }
-            });
-        } catch (IOException e){
-            e.printStackTrace();
-            System.out.println("Could not read directory");
-        }
-    }
-
-    private JPanel initFolders(){
-        JPanel folderView = new JPanel();
-        folderView.setLayout(new BoxLayout(folderView, BoxLayout.Y_AXIS));
-
-
-
-        for(Folder folder : folders){
-            JButton folderButton = new JButton(folder.getFolderName());
-
-
-            //folderButton.setIcon(new ImageIcon(scaler));
-
-            folderButton.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    if(e.getClickCount() == 2){
-                        refreshWindow(folder);
-                    }
-                }
-            });
-            folderView.add(folderButton);
-        }
-
-        return folderView;
-    }
-
-    private JPanel initNotes(){
-        JPanel notesView = new JPanel();
-        notesView.setLayout(new FlowLayout());
-
-        for (Note note : notes){
-            JButton noteButton = new JButton(note.getNoteName());
-
-            ImageIcon imageIcon = new ImageIcon("src/main/resources/md-logo.png");
-            Image scaler = imageIcon.getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
-            noteButton.setIcon(new ImageIcon(scaler));
-
-            noteButton.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    if (e.getClickCount() == 2){
-                        new NoteEditor(note);
-                    }
-                }
-            });
-            notesView.add(noteButton);
-        }
-
-
-        return notesView;
-    }
-
-    /*
-    DEAD CODE ENDS HERE
-     */
 
     private JButton initAddButton(){
         addButton = new JButton();
@@ -237,5 +120,4 @@ public class NoteManager {
         return settingsButton;
     }
 
-    //TODO: add an init function for showing the current directory path (JPanel)
 }
